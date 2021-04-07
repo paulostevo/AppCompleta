08bb92a8c8034f1bec1621340d6e3d9e5fdf91a9
Paulo Stevo <paulostevo@gmail.com>
2021-04-06 21:23:50 -0300

    Initial Project

---
+++ src/DevIO.Business/Interfaces/IfornecedorRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+    interface IfornecedorRepository : IRepository<Fornecedor>
+    {
+
+    }
+}
